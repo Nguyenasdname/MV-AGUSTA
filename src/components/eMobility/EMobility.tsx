e3a5e7c2686c80dd97f2f8c44827ef6d52c1dbba
Hieu Tran Le Trung <HieuTLT1@fpt.com>
2025-07-15 09:19:53 +0700

    create eMobility

--- src/components/eMobility/EMobility.tsx
+++ src/components/eMobility/EMobility.tsx
@@ -1,9 +1,39 @@
-const EMobility = () => {
-    return(
-        <div className="text-dark">
-            This IS eMobility
-        </div>
-    )
+import { useEffect, useState } from "react";
+import EMobilityItem from "../eMobilityItem/EMobilityItem";
+import "./EMobility.css";
+
+interface EMobilityData {
+  title: string;
+  subtitle: string;
+  image: string;
+  link: string;
 }
 
-export default EMobility
+const EMobility = () => {
+  const [items, setItems] = useState<EMobilityData[]>([]);
+
+  useEffect(() => {
+    fetch("/Data/eMobility.json")
+      .then((res) => res.json())
+      .then((data) => setItems(data))
+      .catch((err) => console.error("Failed to load eMobility data", err));
+  }, []);
+
+  return (
+    <div className="section-wrapper">
+      <div className="card-list" data-selected="emobility">
+        {items.map((item, index) => (
+          <EMobilityItem
+            key={index}
+            title={item.title}
+            subtitle={item.subtitle}
+            image={item.image}
+            link={item.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EMobility;
